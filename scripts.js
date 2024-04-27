71a57c86d07cc5a06818c35c50aab06e1fa8225b
Unichain-NW <168258719+Unichain-NW@users.noreply.github.com>
2024-04-27 15:42:24 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 function learnMore() {
-    alert("网站维护中，欢迎后续了解更多关于上海恩吾科技有限公司的信息！");
+    alert("网站维护中，请谅解！欢迎了解更多关于上海恩吾科技有限公司的信息！");
+}
+
+function handleClick() {
+    alert("It's about Fasteners & Package!");
 }
