5abee7437b922b8dde008e4bde6069f163bf7437
Unichain-NW <168258719+Unichain-NW@users.noreply.github.com>
2024-04-27 21:06:50 +0800

    Update scripts.js
    Signed-off-by: Unichain-NW <168258719+Unichain-NW@users.noreply.github.com>

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function learnMore() {
-    alert("网站维护中，请谅解！欢迎了解更多关于上海恩吾科技有限公司的信息！");
+    alert("网站维护中，请谅解！欢迎扫描二维码了解更多关于上海恩吾科技有限公司的信息！");
 }
 
 function handleClick() {
-    alert("It's about Fasteners & Package!");
+    alert("以专业级紧固件和包装产品为载体的供应链数字化商业解决方案提供商");
 }
